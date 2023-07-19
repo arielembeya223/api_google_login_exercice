888f13f11087822d90e5680210c8993ab6d5f460
ariel <arielshongoordi@gmail.com>
2023-07-19 19:57:33 +0200

    second commit

--- connecte.php
+++ connecte.php
@@ -1,4 +1,22 @@
 <?php
 require 'vendor/autoload.php';
- echo $_GET['code'];
+require 'config.php';
+use GuzzleHttp\client;
+$client = new Client([
+    'timeout' => 2.0,
+    'verify' => __DIR__ . './cacert.pem'
+]);
+ $Response = $client->request('GET','https://accounts.google.com/.well-known/openid-configuration');
+ $token = (json_decode((string)($Response->getBody())))->token_endpoint;
+ $Response = $client->request('POST' . $token,[
+    'form_params' =>
+    [
+        'code'=>$_GET['code'],
+        'client_id' => GOOGLE_ID,
+        'client_secret' => GOOGLE_SECRET,
+        'redirect_uri' => 'http://localhost:8000/connecte.php',
+        'gerant_type' => 'autorisation_code'
+    ]
+ ]);
+ dd((string)$Response->getBody());
 ?>
